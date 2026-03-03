1739504bad41685a98c7b49bc4e98d873bf0db21
Devin <duhrich@gmail.com>
2026-03-03 11:13:01 -0700

    Refactor tick to operate on a single item
    - Remove array/for-loop from tick, accept a single item object
    - Update tests to pass plain objects instead of single-element arrays
    - Add @param/@returns JSDoc tags
    - Un-skip Conjured Item tests

--- gilded-rose.js
+++ gilded-rose.js
@@ -1,69 +1,59 @@
 /**
- * The update quality function
+ * Advances an item by one day, updating its sellIn and quality.
+ * @param {{ name: string, sellIn: number, quality: number }} item
+ * @returns {{ name: string, sellIn: number, quality: number }} The same item, mutated.
  * @example
- * tick([
- *   { name: "+5 Dexterity Vest", sellIn: 10, quality: 20 },
- *   { name: "Aged Brie", sellIn: 2, quality: 0 },
- *   { name: "Elixir of the Mongoose", sellIn: 5, quality: 7 },
- *   { name: "Sulfuras, Hand of Ragnaros", sellIn: 0, quality: 80 },
- *   {
- *     name: "Backstage passes to a TAFKAL80ETC concert",
- *     sellIn: 15,
- *     quality: 20
- *   },
- *   { name: "Conjured Mana Cake", sellIn: 3, quality: 6 }
- * ]);
+ * tick({ name: "+5 Dexterity Vest", sellIn: 10, quality: 20 });
+ * // => { name: "+5 Dexterity Vest", sellIn: 9, quality: 19 }
  */
 
-module.exports = function tick(items) {
-  for (var i = 0; i < items.length; i++) {
-    if (
-      items[i].name != "Aged Brie" &&
-      items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-    ) {
-      if (items[i].quality > 0) {
-        if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-          items[i].quality = items[i].quality - 1;
-        }
-      }
-    } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1;
-        if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-          if (items[i].sellIn < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-          if (items[i].sellIn < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1;
-            }
-          }
-        }
+module.exports = function tick(item) {
+  if (
+    item.name != "Aged Brie" &&
+    item.name != "Backstage passes to a TAFKAL80ETC concert"
+  ) {
+    if (item.quality > 0) {
+      if (item.name != "Sulfuras, Hand of Ragnaros") {
+        item.quality = item.quality - 1;
       }
     }
-    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-      items[i].sellIn = items[i].sellIn - 1;
-    }
-    if (items[i].sellIn < 0) {
-      if (items[i].name != "Aged Brie") {
-        if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-          if (items[i].quality > 0) {
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-              items[i].quality = items[i].quality - 1;
-            }
+  } else {
+    if (item.quality < 50) {
+      item.quality = item.quality + 1;
+      if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.sellIn < 11) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
           }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality;
         }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
+        if (item.sellIn < 6) {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
         }
       }
     }
   }
+  if (item.name != "Sulfuras, Hand of Ragnaros") {
+    item.sellIn = item.sellIn - 1;
+  }
+  if (item.sellIn < 0) {
+    if (item.name != "Aged Brie") {
+      if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+        if (item.quality > 0) {
+          if (item.name != "Sulfuras, Hand of Ragnaros") {
+            item.quality = item.quality - 1;
+          }
+        }
+      } else {
+        item.quality = item.quality - item.quality;
+      }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+      }
+    }
+  }
 
-  return items;
+  return item;
 };
